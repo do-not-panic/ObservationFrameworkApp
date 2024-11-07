71820c73e0cc1e42df3db20f85faba1b8ab57dce
do-not-panic <schach@gmail.com>
2024-11-07 07:15:37 +0100

    Initial Commit

---
+++ ObservationFrameworkApp/ObservationFrameworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationFrameworkAppApp.swift
+//  ObservationFrameworkApp
+//
+//  Created by René Pfammatter on 07.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ObservationFrameworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
